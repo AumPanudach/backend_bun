7806ed4fb3972d6db50a08c52619d1daee58882e
AumPanudach <panudach.work@gmail.com>
2024-11-28 10:04:12 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia").listen(3001)
+  .get("/json", () => {
+    return {
+      hello: "world",
+    };
+  })
+  .get("/user/:id",({params:{id}}) => id)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
